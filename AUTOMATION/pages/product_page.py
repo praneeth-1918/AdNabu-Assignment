4006f69273eceb372b05bac25a93ccd60ff054ac
praneeth-1918 <55698766+praneeth-1918@users.noreply.github.com>
2026-04-05 11:12:51 +0530

    Update product_page.py

--- AUTOMATION/pages/product_page.py
+++ AUTOMATION/pages/product_page.py
@@ -1,1 +1,13 @@
+from selenium.webdriver.common.by import By
+from .base_page import BasePage
 
+class ProductPage(BasePage):
+
+add_to_cart_btn = (By.NAME, "add")
+cart_icon = (By.XPATH, "//a[contains(@href,'cart')]")
+
+def add_to_cart(self):
+self.click(self.add_to_cart_btn)
+
+def is_cart_visible(self):
+return self.get_element(self.cart_icon).is_displayed()
