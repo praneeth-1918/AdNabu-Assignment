9cb8f24a9cb9015d4538a25526a183765c38537f
praneeth-1918 <55698766+praneeth-1918@users.noreply.github.com>
2026-04-05 11:13:18 +0530

    Update search_page.py

--- AUTOMATION/pages/search_page.py
+++ AUTOMATION/pages/search_page.py
@@ -1,1 +1,18 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from .base_page import BasePage
 
+class SearchPage(BasePage):
+
+# Locators
+search_icon = (By.XPATH, "//a[contains(@href,'search')]")
+search_box = (By.NAME, "q")
+first_product = (By.CSS_SELECTOR, ".grid__item a")
+
+def search_product(self, product_name):
+self.click(self.search_icon)
+self.send_keys(self.search_box, product_name)
+self.get_element(self.search_box).send_keys(Keys.RETURN)
+
+def select_first_product(self):
+self.click(self.first_product)
