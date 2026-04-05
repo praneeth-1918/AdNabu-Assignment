1e13d9f559881456f63b5ce0ea3fe367728471e6
praneeth-1918 <55698766+praneeth-1918@users.noreply.github.com>
2026-04-05 11:12:02 +0530

    Update base_page.py

--- AUTOMATION/pages/base_page.py
+++ AUTOMATION/pages/base_page.py
@@ -1,1 +1,20 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class BasePage:
+
+def __init__(self, driver):
+self.driver = driver
+self.wait = WebDriverWait(driver, 15)
+
+def click(self, locator):
+self.wait.until(EC.element_to_be_clickable(locator)).click()
+
+def send_keys(self, locator, value):
+element = self.wait.until(EC.presence_of_element_located(locator))
+element.clear()
+element.send_keys(value)
+
+def get_element(self, locator):
+return self.wait.until(EC.presence_of_element_located(locator))
 
