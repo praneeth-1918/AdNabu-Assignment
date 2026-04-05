f4dcc92b46ffeb0b82bb0ce44b849bbb14b8289d
praneeth-1918 <55698766+praneeth-1918@users.noreply.github.com>
2026-04-05 11:12:33 +0530

    Update login_page.py

--- AUTOMATION/pages/login_page.py
+++ AUTOMATION/pages/login_page.py
@@ -1,1 +1,18 @@
+from selenium.webdriver.common.by import By
+from .base_page import BasePage
 
+class LoginPage(BasePage):
+
+# Locators
+password_input = (By.ID, "Password")
+enter_button = (By.XPATH, "//button[@type='submit']")
+
+def enter_password(self, password):
+self.send_keys(self.password_input, password)
+
+def submit_password(self):
+self.click(self.enter_button)
+
+def login(self, password):
+self.enter_password(password)
+self.submit_password()
